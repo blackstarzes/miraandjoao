156db113ad1f3829a0f1fbe216b7ac6f2f0a6a2b
Joao Lourenco <joao@lourenco.id.au>
2019-02-03 13:47:21 +1100

    Added post-rsvp function

---
+++ src/lambda/api/post-rsvp/app.ts
@@ -0,0 +1,129 @@
+import AWS = require('aws-sdk');
+import {APIGatewayEvent, Context, ProxyResult} from "aws-lambda";
+import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
+import QueryInput = DocumentClient.QueryInput;
+
+import {isRsvp, isValidRsvpForUser, Rsvp} from "./miraandjoao-lib/models/rsvp";
+import {User} from "./miraandjoao-lib/models/user";
+import PutItemInput = DocumentClient.PutItemInput;
+
+export const lambdaHandler = async (event: APIGatewayEvent, context: Context) => {
+
+    // AWS services (create here so that aws-sdk-mock works)
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+    const rsvpsTableName: string = process.env.RSVPS_TABLE_NAME!;
+    const usersTableName: string = process.env.USERS_TABLE_NAME!;
+    const usersTableUserTagIndexName: string = process.env.USERS_TABLE_USERTAG_INDEX_NAME!;
+
+    // Prepare the response
+    let response: ProxyResult;
+
+    try {
+        // Extract the user tag from the path
+        const userTag: string = event.pathParameters == null
+            ? ""
+            : event.pathParameters!["usertag"];
+        if (userTag == null || userTag == "") {
+            // Return UserId not provided
+            console.log("UserTag not provided");
+            response = {
+                statusCode: 403,
+                body: JSON.stringify({
+                    message: "UserTag not provided"
+                })
+            };
+        } else {
+            // Validate RSVP structure
+            console.log("Validating RSVP...");
+            console.log(`UserTag: ${userTag}`);
+            console.log(`Payload: ${JSON.stringify(event.body)}`);
+            if (event.body != null && isRsvp(event.body)) {
+                console.log("RSVP valid");
+
+                // Cast payload to RSVP
+                const rsvp: Rsvp = <Rsvp> event.body;
+
+                // Fetch the user
+                console.log("Fetching user from table...");
+                const userQueryParams: QueryInput = {
+                    TableName: usersTableName,
+                    IndexName: usersTableUserTagIndexName,
+                    KeyConditionExpression: "#usertag = :usertag",
+                    ExpressionAttributeNames: {
+                        "#usertag": "usertag"
+                    },
+                    ExpressionAttributeValues: {
+                        ":usertag": userTag
+                    },
+                    Limit: 1
+                };
+                let userQueryResult = await dynamoDb.query(userQueryParams).promise();
+                if (userQueryResult.Count != 0) {
+                    console.log("User found");
+
+                    // Validate RSVP against the User
+                    const user = <User>userQueryResult.Items![0];
+                    console.log("Validating RSVP against User...");
+                    if (isValidRsvpForUser(rsvp, user)) {
+                        console.log("RSVP valid against User");
+
+                        // Overwrite content that is calculated server-side
+                        rsvp.usertag = userTag;
+                        rsvp.timestamp = Date.now();
+                        rsvp.allowchildren = user.allowchildren;
+
+                        // Persist RSVP
+                        const params: PutItemInput = {
+                            TableName: rsvpsTableName,
+                            Item: rsvp
+                        };
+                        await dynamoDb.put(params).promise();
+                        response = {
+                            statusCode: 200,
+                            body: JSON.stringify(rsvp)
+                        };
+                    } else {
+                        console.log("RSVP invalid against User");
+                        response = {
+                            statusCode: 400,
+                            body: JSON.stringify({
+                                message: "RSVP invalid"
+                            })
+                        };
+                    }
+                } else {
+                    console.log("User not found");
+                    response = {
+                        statusCode: 404,
+                        body: JSON.stringify({
+                            message: "UserTag not found"
+                        })
+                    };
+                }
+            } else {
+                console.log("RSVP invalid");
+                response = {
+                    statusCode: 400,
+                    body: JSON.stringify({
+                        message: "RSVP invalid"
+                    })
+                };
+            }
+        }
+    } catch (err) {
+        // Log the error
+        console.log(err);
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: "Unhandled exception"
+            })
+        };
+    }
+
+    response.headers = {
+        "access-control-allow-origin": process.env.HEADER_ACAO!
+    };
+
+    return response;
+};
---
+++ src/lambda/api/post-rsvp/tests/unit/test-handler.ts
@@ -0,0 +1,427 @@
+// Test subject
+import app = require("../../app.js");
+
+// External libraries
+import chai from "chai";
+const createEvent = require("aws-event-mocks");
+import {APIGatewayEvent, Context} from "aws-lambda";
+import AWS from 'aws-sdk-mock';
+import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
+import QueryInput = DocumentClient.QueryInput;
+
+// Local libraries
+import {Rsvp} from "../../miraandjoao-lib/models/rsvp";
+import {User} from "../../miraandjoao-lib/models/user";
+import {Diet} from "../../miraandjoao-lib/models/diet";
+
+// Test framework
+const expect = chai.expect;
+
+// Constants
+const DOMAIN_UI = "https://www.example.com";
+const RSVPS_TABLE_NAME = "rsvp-table";
+const USERS_TABLE_NAME = "user-table";
+const USERS_TABLE_USERTAG_INDEX_NAME = "user-table-usertag-index";
+let context: Context;
+
+// Tests
+describe("Post RSVP tests", function () {
+    it("When UserTag is not provided, should return 403 with message 'UserTag not provided'", async () => {
+        // Given
+        process.env.HEADER_ACAO = DOMAIN_UI;
+        let event: APIGatewayEvent = createEvent({
+            template: "aws:apiGateway"
+        });
+
+        // When
+        const result = await app.lambdaHandler(event, context);
+
+        // Then
+        expect(result).to.be.an("object");
+        expect(result.statusCode).to.equal(403);
+        expect(result.body).to.be.an("string");
+        expect(result.headers).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.be.equal(DOMAIN_UI);
+        let response = JSON.parse(result.body);
+        expect(response).to.be.an("object");
+        expect(response.message).to.be.equal("UserTag not provided");
+    });
+
+    it("When UserTag is provided and no payload is provided, should return 400 with message 'RSVP invalid'", async () => {
+        // Given
+        process.env.RSVPS_TABLE_NAME = RSVPS_TABLE_NAME;
+        process.env.USERS_TABLE_NAME = USERS_TABLE_NAME;
+        process.env.USERS_TABLE_USERTAG_INDEX_NAME = USERS_TABLE_USERTAG_INDEX_NAME;
+        AWS.mock("DynamoDB.DocumentClient", "query", function (params: QueryInput, callback: any) {
+            if (params.TableName == USERS_TABLE_NAME
+                && params.IndexName == USERS_TABLE_USERTAG_INDEX_NAME
+                && params.KeyConditionExpression == "#usertag = :usertag"
+                && params.ExpressionAttributeNames!.hasOwnProperty("#usertag")
+                && params.ExpressionAttributeNames!["#usertag"] == "usertag"
+                && params.ExpressionAttributeValues!.hasOwnProperty(":usertag")
+                && params.ExpressionAttributeValues![":usertag"] == "abc123"
+                && params.Limit == 1) {
+                const response: DocumentClient.QueryOutput = {
+                    Count: 0,
+                    Items: []
+                };
+                callback(null, response);
+            }
+        });
+        let event: APIGatewayEvent = createEvent({
+            template: "aws:apiGateway",
+            merge: {
+                pathParameters: {
+                    "usertag": "abc123"
+                }
+            }
+        });
+
+        // When
+        const result = await app.lambdaHandler(event, context);
+
+        // Then
+        expect(result).to.be.an("object");
+        expect(result.statusCode).to.equal(400);
+        expect(result.body).to.be.an("string");
+        expect(result.headers).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.be.equal(DOMAIN_UI);
+        let response = JSON.parse(result.body);
+        expect(response).to.be.an("object");
+        expect(response.message).to.be.equal("RSVP invalid");
+
+        // Cleanup
+        AWS.restore("DynamoDB.DocumentClient");
+    });
+
+    it("When UserTag is provided and payload is the wrong structure, should return 400 with message 'RSVP invalid'", async () => {
+        // Given
+        process.env.RSVPS_TABLE_NAME = RSVPS_TABLE_NAME;
+        process.env.USERS_TABLE_NAME = USERS_TABLE_NAME;
+        process.env.USERS_TABLE_USERTAG_INDEX_NAME = USERS_TABLE_USERTAG_INDEX_NAME;
+        AWS.mock("DynamoDB.DocumentClient", "query", function (params: QueryInput, callback: any) {
+            if (params.TableName == USERS_TABLE_NAME
+                && params.IndexName == USERS_TABLE_USERTAG_INDEX_NAME
+                && params.KeyConditionExpression == "#usertag = :usertag"
+                && params.ExpressionAttributeNames!.hasOwnProperty("#usertag")
+                && params.ExpressionAttributeNames!["#usertag"] == "usertag"
+                && params.ExpressionAttributeValues!.hasOwnProperty(":usertag")
+                && params.ExpressionAttributeValues![":usertag"] == "abc123"
+                && params.Limit == 1) {
+                const response: DocumentClient.QueryOutput = {
+                    Count: 0,
+                    Items: []
+                };
+                callback(null, response);
+            }
+        });
+        let event: APIGatewayEvent = createEvent({
+            template: "aws:apiGateway",
+            merge: {
+                body: {
+                    "junk": "junk"
+                },
+                pathParameters: {
+                    "usertag": "abc123"
+                }
+            }
+        });
+
+        // When
+        const result = await app.lambdaHandler(event, context);
+
+        // Then
+        expect(result).to.be.an("object");
+        expect(result.statusCode).to.equal(400);
+        expect(result.body).to.be.an("string");
+        expect(result.headers).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.be.equal(DOMAIN_UI);
+        let response = JSON.parse(result.body);
+        expect(response).to.be.an("object");
+        expect(response.message).to.be.equal("RSVP invalid");
+
+        // Cleanup
+        AWS.restore("DynamoDB.DocumentClient");
+    });
+
+    it("When UserTag is provided, payload is the correct structure and User does not exist, should return 404 with message 'UserTag not found'", async () => {
+        // Given
+        process.env.RSVPS_TABLE_NAME = RSVPS_TABLE_NAME;
+        process.env.USERS_TABLE_NAME = USERS_TABLE_NAME;
+        process.env.USERS_TABLE_USERTAG_INDEX_NAME = USERS_TABLE_USERTAG_INDEX_NAME;
+        AWS.mock("DynamoDB.DocumentClient", "query", function (params: QueryInput, callback: any) {
+            if (params.TableName == USERS_TABLE_NAME
+                && params.IndexName == USERS_TABLE_USERTAG_INDEX_NAME
+                && params.KeyConditionExpression == "#usertag = :usertag"
+                && params.ExpressionAttributeNames!.hasOwnProperty("#usertag")
+                && params.ExpressionAttributeNames!["#usertag"] == "usertag"
+                && params.ExpressionAttributeValues!.hasOwnProperty(":usertag")
+                && params.ExpressionAttributeValues![":usertag"] == "abc123"
+                && params.Limit == 1) {
+                const response: DocumentClient.QueryOutput = {
+                    Count: 0,
+                    Items: []
+                };
+                callback(null, response);
+            }
+        });
+        const rsvp: any = {
+            allowchildren: true,
+            bacheloretteparty: true,
+            bachelorparty: true,
+            people: [{
+                name: "John",
+                diet: Diet.Standard,
+                rsvpResponse: true
+            }, {
+                name: "Jane",
+                diet: Diet.Vegetarian,
+                rsvpResponse: true
+            }],
+            timestamp: 123456,
+            usertag: "abc123"
+        };
+        let event: APIGatewayEvent = createEvent({
+            template: "aws:apiGateway",
+            merge: {
+                body: rsvp,
+                pathParameters: {
+                    "usertag": "abc123"
+                }
+            }
+        });
+
+        // When
+        const result = await app.lambdaHandler(event, context);
+
+        // Then
+        expect(result).to.be.an("object");
+        expect(result.statusCode).to.equal(404);
+        expect(result.body).to.be.an("string");
+        expect(result.headers).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.be.equal(DOMAIN_UI);
+        let response = JSON.parse(result.body);
+        expect(response).to.be.an("object");
+        expect(response.message).to.be.equal("UserTag not found");
+
+        // Cleanup
+        AWS.restore("DynamoDB.DocumentClient");
+    });
+
+    it("When UserTag is provided, payload is the correct structure, invalid for the user and User does exist, should return 400 with message 'RSVP invalid'", async () => {
+        // Given
+        process.env.RSVPS_TABLE_NAME = RSVPS_TABLE_NAME;
+        process.env.USERS_TABLE_NAME = USERS_TABLE_NAME;
+        process.env.USERS_TABLE_USERTAG_INDEX_NAME = USERS_TABLE_USERTAG_INDEX_NAME;
+        AWS.mock("DynamoDB.DocumentClient", "query", function (params: QueryInput, callback: any) {
+            if (params.TableName == RSVPS_TABLE_NAME
+                && params.KeyConditionExpression == "#usertag = :usertag"
+                && params.ExpressionAttributeNames!.hasOwnProperty("#usertag")
+                && params.ExpressionAttributeNames!["#usertag"] == "usertag"
+                && params.ExpressionAttributeValues!.hasOwnProperty(":usertag")
+                && params.ExpressionAttributeValues![":usertag"] == "abc123"
+                && params.ScanIndexForward
+                && params.Limit == 1) {
+                const response: DocumentClient.QueryOutput = {
+                    Count: 0,
+                    Items: []
+                };
+                callback(null, response);
+            } else if (params.TableName == USERS_TABLE_NAME
+                && params.IndexName == USERS_TABLE_USERTAG_INDEX_NAME
+                && params.KeyConditionExpression == "#usertag = :usertag"
+                && params.ExpressionAttributeNames!.hasOwnProperty("#usertag")
+                && params.ExpressionAttributeNames!["#usertag"] == "usertag"
+                && params.ExpressionAttributeValues!.hasOwnProperty(":usertag")
+                && params.ExpressionAttributeValues![":usertag"] == "abc123"
+                && params.Limit == 1) {
+                const user: User = {
+                    allowchildren: true,
+                    delivered: {
+                        savethedate: true
+                    },
+                    invitedcount: 2,
+                    invitednames: "John and Jane",
+                    language: "en",
+                    rsvpcount: 0,
+                    salutation: "Dear",
+                    userid: "johnandjane@example.com",
+                    usertag: "abc123"
+                };
+                const response: DocumentClient.QueryOutput = {
+                    Count: 1,
+                    Items: [user]
+                };
+                callback(null, response);
+            }
+        });
+        AWS.mock("DynamoDB.DocumentClient", "put", function (params: QueryInput, callback: any) {
+            if (params.TableName == RSVPS_TABLE_NAME) {
+                const response: DocumentClient.PutItemOutput = {};
+                callback(null, response);
+            }
+        });
+        const rsvp: any = {
+            allowchildren: true,
+            bacheloretteparty: true,
+            bachelorparty: true,
+            people: [{
+                name: "John",
+                diet: Diet.Standard,
+                rsvpResponse: true
+            }, {
+                name: "Jen",
+                diet: Diet.Vegetarian,
+                rsvpResponse: true,
+                allergies: "Peanuts"
+            }],
+            timestamp: 123456,
+            usertag: "abc123"
+        };
+        let event: APIGatewayEvent = createEvent({
+            template: "aws:apiGateway",
+            merge: {
+                body: rsvp,
+                pathParameters: {
+                    "usertag": "abc123"
+                }
+            }
+        });
+
+        // When
+        const result = await app.lambdaHandler(event, context);
+
+        // Then
+        expect(result).to.be.an("object");
+        expect(result.statusCode).to.equal(400);
+        expect(result.body).to.be.an("string");
+        expect(result.headers).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.be.equal(DOMAIN_UI);
+        let response = JSON.parse(result.body);
+        expect(response).to.be.an("object");
+        expect(response.message).to.be.equal("RSVP invalid");
+
+        // Cleanup
+        AWS.restore("DynamoDB.DocumentClient");
+    });
+
+    it("When UserTag is provided, payload is the correct structure, valid for the user and User does exist, should return 200 with RSVP as the body", async () => {
+        // Given
+        process.env.RSVPS_TABLE_NAME = RSVPS_TABLE_NAME;
+        process.env.USERS_TABLE_NAME = USERS_TABLE_NAME;
+        process.env.USERS_TABLE_USERTAG_INDEX_NAME = USERS_TABLE_USERTAG_INDEX_NAME;
+        AWS.mock("DynamoDB.DocumentClient", "query", function (params: QueryInput, callback: any) {
+            if (params.TableName == RSVPS_TABLE_NAME
+                && params.KeyConditionExpression == "#usertag = :usertag"
+                && params.ExpressionAttributeNames!.hasOwnProperty("#usertag")
+                && params.ExpressionAttributeNames!["#usertag"] == "usertag"
+                && params.ExpressionAttributeValues!.hasOwnProperty(":usertag")
+                && params.ExpressionAttributeValues![":usertag"] == "abc123"
+                && params.ScanIndexForward
+                && params.Limit == 1) {
+                const response: DocumentClient.QueryOutput = {
+                    Count: 0,
+                    Items: []
+                };
+                callback(null, response);
+            } else if (params.TableName == USERS_TABLE_NAME
+                && params.IndexName == USERS_TABLE_USERTAG_INDEX_NAME
+                && params.KeyConditionExpression == "#usertag = :usertag"
+                && params.ExpressionAttributeNames!.hasOwnProperty("#usertag")
+                && params.ExpressionAttributeNames!["#usertag"] == "usertag"
+                && params.ExpressionAttributeValues!.hasOwnProperty(":usertag")
+                && params.ExpressionAttributeValues![":usertag"] == "abc123"
+                && params.Limit == 1) {
+                const user: User = {
+                    allowchildren: true,
+                    delivered: {
+                        savethedate: true
+                    },
+                    invitedcount: 2,
+                    invitednames: "John and Jane",
+                    language: "en",
+                    rsvpcount: 0,
+                    salutation: "Dear",
+                    userid: "johnandjane@example.com",
+                    usertag: "abc123"
+                };
+                const response: DocumentClient.QueryOutput = {
+                    Count: 1,
+                    Items: [user]
+                };
+                callback(null, response);
+            }
+        });
+        AWS.mock("DynamoDB.DocumentClient", "put", function (params: QueryInput, callback: any) {
+            if (params.TableName == RSVPS_TABLE_NAME) {
+                const response: DocumentClient.PutItemOutput = {};
+                callback(null, response);
+            }
+        });
+        const rsvp: any = {
+            allowchildren: true,
+            bacheloretteparty: true,
+            bachelorparty: true,
+            people: [{
+                name: "John",
+                diet: Diet.Standard,
+                rsvpResponse: true
+            }, {
+                name: "Jane",
+                diet: Diet.Vegetarian,
+                rsvpResponse: true,
+                allergies: "Peanuts"
+            }],
+            timestamp: 123456,
+            usertag: "abc123"
+        };
+        let event: APIGatewayEvent = createEvent({
+            template: "aws:apiGateway",
+            merge: {
+                body: rsvp,
+                pathParameters: {
+                    "usertag": "abc123"
+                }
+            }
+        });
+
+        // When
+        const result = await app.lambdaHandler(event, context);
+
+        // Then
+        expect(result).to.be.an("object");
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.be.an("string");
+        expect(result.headers).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.not.be.null;
+        expect(result.headers!["access-control-allow-origin"]).to.be.equal(DOMAIN_UI);
+        let response = JSON.parse(result.body);
+        expect(response).to.be.an("object");
+        let typedResponse = <Rsvp> response;
+        expect(typedResponse.allowchildren).to.be.equal(true);
+        expect(typedResponse.bacheloretteparty).to.be.true;
+        expect(typedResponse.bachelorparty).to.be.true;
+        expect(typedResponse.people).to.not.be.null;
+        expect(typedResponse.people.length).to.be.equal(2);
+        expect(typedResponse.people[0]).to.not.be.null;
+        expect(typedResponse.people[0].name).to.be.equal("John");
+        expect(typedResponse.people[0].diet).to.be.equal(Diet.Standard);
+        expect(typedResponse.people[0].rsvpResponse).to.be.true;
+        expect(typedResponse.people[0].allergies).to.be.undefined;
+        expect(typedResponse.people[1]).to.not.be.null;
+        expect(typedResponse.people[1].name).to.be.equal("Jane");
+        expect(typedResponse.people[1].diet).to.be.equal(Diet.Vegetarian);
+        expect(typedResponse.people[1].rsvpResponse).true;
+        expect(typedResponse.people[1].allergies).to.be.equal("Peanuts");
+        expect(typedResponse.timestamp).to.approximately(Date.now(), 1000);
+        expect(typedResponse.usertag).to.be.equal("abc123");
+
+        // Cleanup
+        AWS.restore("DynamoDB.DocumentClient");
+    });
+});
