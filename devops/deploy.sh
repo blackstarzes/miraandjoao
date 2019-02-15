3e4b305977c13ff2f81cc70eedaa4267fd46be95
Joao Lourenco <joao@lourenco.id.au>
2019-02-16 08:10:58 +1100

    Added deploy script to update SES templates

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -5,3 +5,11 @@ echo 'Deploying static files...'
 aws s3 sync build/app/ s3://miraandjoao.com/ --delete
 echo 'Invalidating CloudFront cache...'
 aws cloudfront create-invalidation --distribution-id E1K272H4VCI0W5 --paths "/*"
+
+echo 'Deploying email templates...'
+for template in $(find build/email -name *-template.json)
+do
+    echo 'Updating ' $template
+    # aws ses create-template --cli-input-json file://$template
+    aws ses update-template --cli-input-json file://$template
+done
